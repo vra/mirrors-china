b501452b57e5567c8f12abefd14e6a72890c7e12
vra <wangyf11@mail.ustc.edu.cn>
2018-04-18 15:39:45 +0800

    add config files

---
+++ configs/conda.sh
@@ -0,0 +1,9 @@
+# ustc, doc: https://mirrors.ustc.edu.cn/help/anaconda.html
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
+
+# thu, doc: https://mirror.tuna.tsinghua.edu.cn/help/anaconda/
+#conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+#conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+#conda config --set show_channel_urls yes
---
+++ configs/rubygems.sh
@@ -0,0 +1,8 @@
+# ustc, doc: https://mirrors.ustc.edu.cn/help/rubygems.html
+gem sources  #列出默认源
+gem sources --remove https://rubygems.org/  #移除默认源
+gem sources -a https://mirrors.ustc.edu.cn/rubygems/  #添加科大源
+
+# thu, doc: https://mirror.tuna.tsinghua.edu.cn/help/rubygems/
+#gem sources --add https://mirrors.tuna.tsinghua.edu.cn/rubygems/ --remove #https://rubygems.org/
+#gem sources -l
